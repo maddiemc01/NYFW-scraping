8aefe6a10a6ac475c292b2d037c29f3f9bb0f963
Madison McCardle <madison.mccardle@powerhrg.com>
2020-05-18 13:18:31 -0400

    create gemfile

--- lib/scraping.rb
+++ lib/scraping.rb
@@ -4,6 +4,7 @@ class Scraping
         index_page = Nokogiri::HTML(open(https://nyfw.com/schedule/))
         events_array = []
         events = index_page.css("component-inner media-b24485da media-74b33025") && index_page.css("component-inner media-b24485da media-74b33025 media-31620920")
+        events_array << events
     end
 
     def scrape_details
--- lib/user.rb
+++ lib/user.rb
@@ -4,9 +4,10 @@ class user
     # 1 pick a valid date
     def valid_day(day)
          day = (4..12)
+         binding.pry
     end
 
-    def day (day)
+    def event (day)
         puts "please enter a day of february, ranging from the 4th to the 12th."
         input = gets.stip
         if valid_date && input = 4
@@ -17,5 +18,5 @@ class user
 
 
     # 3 pick an event
-
+        def event
     # 4 scape bio info
