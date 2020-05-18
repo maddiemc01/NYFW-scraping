368de51a0309a8b64b17195aa4f60100627712a7
Madison McCardle <madison.mccardle@powerhrg.com>
2020-05-18 12:11:38 -0400

    scrape events

--- lib/user.rb
+++ lib/user.rb
@@ -2,14 +2,19 @@ class user
 
 
     # 1 pick a valid date
-    def valid_date
-         4 - 12
+    def valid_date(day)
+         day = (4..12)
     end
 
-    def date (date)
-        input = gets.sti
+    def date (day)
+        puts "please enter a day of february, ranging from the 4th to the 12th."
+        input = gets.stip
+        if valid_date
+            # display event(s) for that date
+        else
+            puts "sorry, there are no events for that day, please enter a different day"
+        end
 
-    # 2 scrape event(s) under that date
 
     # 3 pick an event
 
