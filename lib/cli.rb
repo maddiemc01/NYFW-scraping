d0b3cf271995600b35d2b30e89e54a42b5438dfa
Madison McCardle <madison.mccardle@powerhrg.com>
2020-05-18 15:42:48 -0400

    files

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,33 +6,9 @@
 class Cli
 
   def call
-    puts "please enter a day of february, ranging from the 4th to the 12th."
-   # binding.pry
+  puts "Welcome fashionista/o! We have a list of articles that has all the latest fashion trends. Please select an article, 1-9, to get more details"
+  binding.pry
   end
-
-  def valid_day(day)
-    day = (4..12)
-    binding.pry
-  end
-
-  def events(day)
-    input = gets.stip
-    if input == 4
-      # display M MISSONI
-    elsif input == 5
-      #display L'AGNENCE
-    elsif input == 6
-      #display MUKZIN x HARBIN, FASHION HONG KONG, TADASHI SHOJI, CHRISTIAN SIRIANO, VISTOR GLEMAUD, OQLIQ
-    elsif input == 7
-    else
-      puts "sorry, there are no events for that day, please enter a different day"
-    end
-  end
-
-  def info
-    puts "please select an event to learn more about it"
-  end
-
 
 end
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,17 +1,4 @@
 #no "puts"
-
-
 class Scraping
-
-    def scrape_events()
-        index_page = Nokogiri::(HTTParty.get("https://nyfw.com/schedule/"))
-        all_events = []
-        events = index_page.css("component-inner media-b24485da media-74b33025") && index_page.css("component-inner media-b24485da media-74b33025 media-31620920")
-        all_events << events
-        binding.pry
-    end
-
-    def scrape_details
-    end
 
 end
